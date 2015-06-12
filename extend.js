d540ae6bc80dc181418f9146be6283e6876649b2
root <root@xiandusi.com>
2015-06-12 15:43:04 +0800

    add extend

---
+++ extend.js
@@ -0,0 +1,50 @@
+//object.create¶Ôie8ÏÂµÄ¼æÈÝ
+if(!Object.create){
+    Object.create = (function(){
+        var F = function(){}
+	return function(o){
+		F.prototype = o;
+                return new F();
+       }
+    })();
+}
+
+function Class() {}
+
+
+Class.prototype.extend = function () {};
+Class.prototype.create = function () {};
+
+
+Class.extend = function (props) {
+    return this.prototype.extend.call(this, props);
+}
+
+Class.prototype.create = function (props) {
+
+    var instance = new this();
+
+    for (var name in props) {
+        instance[name] = props[name];
+    }
+    return instance;
+}
+
+Class.prototype.extend = function (props) {
+
+    var SubClass = function () {};
+
+    SubClass.prototype = Object.create(this.prototype);
+
+    for (var name in props) {
+        SubClass.prototype[name] = props[name];
+    }
+    SubClass.prototype.constructor = SubClass;
+
+
+    SubClass.extend = SubClass.prototype.extend;
+    SubClass.create = SubClass.prototype.create;
+
+    return SubClass;
+}
+
